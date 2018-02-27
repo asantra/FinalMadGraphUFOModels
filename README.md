49d02a8e49ac94ed4ecd2aca66b07c5dedcb0f40
asantra <arka@arka-XPS-L521X>
2018-02-27 15:53:51 +0100

    FinalVersion

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the repository of MadGraph UFO models. These models are well tested for all spin cases (0, 1/2 and 1) and all processes (photon fusion and Drell-Yan).
---
+++ mono_spinhalf_beta/Fortran/functions.f
@@ -0,0 +1,26 @@
+c Momenta of particles in event, written by the user
+
+      REAL*8 FUNCTION mymdl_FormFactor(AAA)
+      IMPLICIT NONE
+      INCLUDE '../genps.inc'
+      COMMON/momenta_pp/pp
+      INCLUDE 'input.inc' ! include all model parameter
+      REAL*8 pp(0:3,max_particles), DOT
+      REAL*8 AAA, ENERGYSQUARE
+      REAL*8 PSQUARE, OUTVAL
+      REAL*8 SHAT
+
+      PSQUARE = pp(1,2)*pp(1,2)+pp(2,2)*pp(2,2)+pp(3,2)*pp(3,2)
+      ENERGYSQUARE  = pp(0,3)*pp(0,3)
+      SHAT = 2*DOT(pp(0,1),pp(0,2))
+      mymdl_FormFactor = SQRT(1 - 4*AAA*AAA/SHAT)  ! keep this form before I find out what is wrong with the compiler
+      !mymdl_FormFactor = SQRT(1.0 - AAA*AAA/(ENERGYSQUARE))  ! beta = sqrt(1 - mass^2/energy^2)
+      OUTVAL = mymdl_FormFactor
+      !PRINT *, 'PSQUARE:', PSQUARE
+      !PRINT *, 'MASS: ', AAA
+      !PRINT *, 'SHAT: ', SHAT
+      PRINT *, 'Form Factor:', OUTVAL
+      !PRINT *, 'ENERGYSQUARE: ', ENERGYSQUARE 
+      
+      RETURN
+      END
